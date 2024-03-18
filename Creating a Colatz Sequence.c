d97b5cbce2be5f1bc3710cca3e66d2f2fcde6fc5
Inters5050 <isi9090@naver.com>
2024-03-18 14:22:31 +0900

    Creating a Colatz Sequence.c

---
+++ Creating a Colatz Sequence.c
@@ -0,0 +1,31 @@
+/*
+모든 자연수 x에 대해서 현재 값이 x이면 x가 짝수일 때는 2로 나누고, x가 홀수일 때는 3 * x + 1로 바꾸는 계산을 계속해서 반복하면 언젠가는 반드시 x가 1이 되는지 묻는 문제를 콜라츠 문제라고 부릅니다.
+
+그리고 위 과정에서 거쳐간 모든 수를 기록한 수열을 콜라츠 수열이라고 부릅니다.
+
+계산 결과 1,000 보다 작거나 같은 수에 대해서는 전부 언젠가 1에 도달한다는 것이 알려져 있습니다.
+
+임의의 1,000 보다 작거나 같은 양의 정수 n이 주어질 때 초기값이 n인 콜라츠 수열을 return 하는 solution 함수를 완성해 주세요.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int)*10000);
+    answer[0] = n;
+    for(int i = 1; i < 1000;i++){
+        if(n == 1){
+            break;
+        }else if(n%2 == 1){
+            n = 3*n+1;
+            answer[i] = n;
+        }else if(n%2 == 0){
+            n = n/2;
+            answer[i] = n;
+        }
+    }
+    return answer;
+}
