e560b58ab47dea01d1909d9586cf9d0e452c2a6b
Inters5050 <isi9090@naver.com>
2024-03-18 14:22:54 +0900

    remainder divided by 9.c

---
+++ remainder divided by 9.c
@@ -0,0 +1,16 @@
+/*음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+int solution(const char* number) {
+    int len = 0;
+    len = strlen(number);
+    int result = 0;
+    for (int i = 0; i < len; i++){
+        result += number[i] - '0';
+    }
+    return result % 9;
+}
